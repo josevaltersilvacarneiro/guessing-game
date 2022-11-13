eabeb840fa4e3c672a2752dd12bec8770f4dde47
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-13 18:25:51 -0300

    Starting

---
+++ jogo-da-velha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+	printf("#######################################\n");
+	printf("Welcome to the guessing game\n");
+	printf("#######################################\n");
+
+	return 0;
+}
