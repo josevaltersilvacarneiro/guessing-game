f5b18f18769aa5bee8b1421cf64a70ed24e8276c
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-13 18:38:18 -0300

    The code was refactored and therefore the print_header function was created

--- jogo-da-velha.c
+++ jogo-da-velha.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
-int
-main(void)
+void
+print_header(void)
 {
+
+	// Print the header of the game
+	
 	printf("#######################################\n");
 	printf("Welcome to the guessing game\n");
 	printf("#######################################\n");
+
+}
+
+int
+main(void)
+{
+	// Variables
+	
+	int secret_number = 42;
+
+	// Start
+	
+	print_header();
 
 	return 0;
 }
